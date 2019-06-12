ade427d8df0a8348efcb8aa40ade3adf91fc5e69
qiangmin <qiangminmail@gmail.com>
2019-06-12 14:57:09 +0800

    add

---
+++ config/config.js
@@ -0,0 +1,14 @@
+export default {
+    plugins:[
+        ['umi-plugin-react',{
+            antd:true
+        }]
+
+    ],
+    routes:[
+        {
+            path:"/",
+            component:"./HelloWorld",
+        }
+    ]
+};
